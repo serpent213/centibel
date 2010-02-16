6afce76f15884ec830ea27b79d728dfbce7ecac6
Steffen Beyer <serpent@tribe-one.org>
2010-02-16 08:41:06 +0000

    integrated tslib, uvesafb

---
+++ conf/tslib.sh
@@ -0,0 +1,6 @@
+# taken from <http://www.armadeus.com/wiki/index.php?title=Tslib>
+
+export TSLIB_TSDEVICE=/dev/input/event3
+export TSLIB_TSEVENTTYPE=INPUT
+export TSLIB_CONFFILE=/etc/ts.conf
+export TSLIB_CALIBFILE=/etc/pointercal
